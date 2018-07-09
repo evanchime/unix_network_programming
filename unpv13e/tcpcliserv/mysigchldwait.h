047b33f8e7153b5d542e0619b9e5a6c98a85b0a6
evan <evanchime@yahoo.co.uk>
2018-07-09 21:18:49 +0100

    added some files necessary for studying chapter 5. also made tcpcliserv folder

---
+++ unpv13e/tcpcliserv/mysigchldwait.h
@@ -0,0 +1,14 @@
+#include	"unp.h"
+
+void
+sig_chld(int signo)
+{
+	pid_t	pid;
+	int		stat;
+
+	/*pid = wait(&stat);
+	printf("child %d terminated\n", pid);*/
+	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)  //version that calls waitpid      
+    	printf("child %d terminated\n", pid);
+	return;
+}
