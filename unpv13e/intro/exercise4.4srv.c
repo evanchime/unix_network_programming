2a07b6f153d725d739ade8e394854727d4b9307e
evan <evanchime@yahoo.co.uk>
2018-07-09 17:19:13 +0100

    Added exercise4.4srv.c and exercise4.4cli.c for exercise 4.4

---
+++ unpv13e/intro/exercise4.4srv.c
@@ -0,0 +1,43 @@
+/*
+    *In Figure 4.11, first change the server's port from 13 to 9999 
+    *(so that we do not need super-user privileges to start the program). 
+    *Remove the call to listen. What happens? 
+*/
+
+#include	"unp.h"
+#include	<time.h>
+
+int
+main(int argc, char **argv)
+{
+	int					listenfd, connfd;
+	socklen_t			len;
+	struct sockaddr_in	servaddr, cliaddr;
+	char				buff[MAXLINE];
+	time_t				ticks;
+
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(9999);	/* daytime server */
+
+	//Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+	Listen(listenfd, LISTENQ);
+
+	for ( ; ; ) {
+		len = sizeof(cliaddr);
+		connfd = Accept(listenfd, (SA *) &cliaddr, &len);
+		printf("connection from %s, port %d\n",
+			   Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)),
+			   ntohs(cliaddr.sin_port));
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+
+		Close(connfd);
+	}
+}
