f5cbac6377403256297ddbd51ebac528681e6097
evan <evanchime@yahoo.co.uk>
2018-07-19 18:07:41 +0100

    modified mysigchldwait.h. just discarded the redundant whitespaces

--- unpv13e/tcpcliserv/mysigchldwait.h
+++ unpv13e/tcpcliserv/mysigchldwait.h
@@ -4,11 +4,11 @@ void
 sig_chld(int signo)
 {
 	pid_t	pid;
-	int		stat;
+	int	stat;
 
 	/*pid = wait(&stat);
 	printf("child %d terminated\n", pid);*/
 	while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)  //version that calls waitpid      
-    	printf("child %d terminated\n", pid);
+    		printf("child %d terminated\n", pid);
 	return;
 }
